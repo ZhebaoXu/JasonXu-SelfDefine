c958526cb27e2a61bdefaefcd14676719b0b9e81
Zhebao <zhierfeifei@126.com>
2016-06-21 16:53:20 +0800

    一个自定义view group，类似线性布局

---
+++ JasonXu-CustomSimpleLayout/app/src/main/java/jasonxu/jasonxu_customsimplelayout/View/MyLinearLayout.java
@@ -0,0 +1,85 @@
+package jasonxu.jasonxu_customsimplelayout.View;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by t_xuz on 6/21/16.
+ */
+public class MyLinearLayout extends ViewGroup {
+
+    public MyLinearLayout(Context context) {
+        super(context);
+    }
+
+    public MyLinearLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyLinearLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthMeasureSize = MeasureSpec.getSize(widthMeasureSpec);
+        int widthMeasureMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMeasureSize = MeasureSpec.getSize(heightMeasureSpec);
+        int heightMeasureMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        //测量模式为AT_MOST的情况
+        int childCount = getChildCount();
+        int width = 0;
+        int height = 0;
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            //让子控件自己去测量一下自己的大小,然后ViewGroup就可以根据子控件的大小来确定测量大小
+            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+
+            MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+            int childWidth = child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+            int childHeight = child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
+
+            width = Math.max(childWidth, width);
+            height += childHeight;
+        }
+
+        setMeasuredDimension((widthMeasureMode == MeasureSpec.EXACTLY) ? widthMeasureSize : width,
+                (heightMeasureMode == MeasureSpec.EXACTLY) ? heightMeasureSize : height);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int childCount = getChildCount();
+        int top = 0;
+
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+
+            MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+            int childWidth = child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+            int childHeight = child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
+            child.layout(0, top, childWidth, childHeight + top);
+            top += childHeight;
+        }
+    }
+
+    @Override
+    protected LayoutParams generateLayoutParams(LayoutParams p) {
+        return new MarginLayoutParams(p);
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
+    }
+
+    @Override
+    protected LayoutParams generateDefaultLayoutParams() {
+        return new MarginLayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+    }
+}
